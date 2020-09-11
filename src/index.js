5b16acfc8a92fb0f5875c96adc31fccf93ec93cf
ArturStahov <vtemy.at.ua@gmail.com>
2020-09-11 22:31:06 +0300

    v1.2 add note item element setting

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import { CreateNote } from './js/createNote'
 import templateNoteItems from './template/template_note_item.hbs'
 import Sortable from 'sortablejs';
 
-
 const refs = {
     form: document.querySelector('[data-form-create-note]'),
     formInputTitle: document.querySelector('[data-input-title]'),
     formInputMessage: document.querySelector('[data-input-message]'),
     noteList: document.querySelector('[data-note-list]'),
-    pageMain: document.querySelector(".content-wrapper")
+    pageMain: document.querySelector(".content-wrapper"),
 }
-
 
 const sort = () => {
     const sortlist = Sortable.create(refs.noteList, { handle: ".note-item_sort-lable", animation: 300, easing: "cubic-bezier(1, 0, 0, 1)" });
@@ -39,9 +37,6 @@ const saveNote = () => {
     localStorage.setItem("saveNote", savestring)
 }
 
-
-
-
 const handlerSubmitForm = (event) => {
     event.preventDefault();
     const noteObj = new CreateNote(refs.formInputTitle.value, refs.formInputMessage.value)
@@ -55,15 +50,40 @@ const handlerSubmitForm = (event) => {
 }
 
 const handlerDeleteNote = (event) => {
-    refs.noteList.removeChild(event.target.parentNode)
-    const noteArr = refs.noteList.querySelectorAll(".note-item")
-    if (noteArr.length === 0) {
-        refs.pageMain.classList.remove("content-padding")
-        localStorage.removeItem("saveNote");
-        return;
-    }
-    saveNote()
+    if (event.target.nodeName === "LI") {
+        const itemRef = event.target.parentNode.parentNode.parentNode;
 
+        if (event.target.dataset.button === "button-delete-item") {
+            refs.noteList.removeChild(itemRef)
+            const noteArr = refs.noteList.querySelectorAll(".note-item")
+            if (noteArr.length === 0) {
+                refs.pageMain.classList.remove("content-padding")
+                localStorage.removeItem("saveNote");
+                return
+            }
+            saveNote()
+        }
+        if (event.target.dataset.tooltip === "green") {
+            itemRef.classList.add("green")
+            itemRef.classList.remove("grey")
+            itemRef.classList.remove("yellow")
+            saveNote()
+        }
+        if (event.target.dataset.tooltip === "grey") {
+            itemRef.classList.add("grey")
+            itemRef.classList.remove("green")
+            itemRef.classList.remove("yellow")
+            saveNote()
+        }
+        if (event.target.dataset.tooltip === "yellow") {
+            itemRef.classList.add("yellow")
+            itemRef.classList.remove("grey")
+            itemRef.classList.remove("green")
+            saveNote()
+        }
+    }
+
+    return;
 }
 
 refs.noteList.addEventListener("click", handlerDeleteNote);
